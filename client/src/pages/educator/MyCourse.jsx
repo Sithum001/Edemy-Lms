6ddd5a0378e93a18fb83aae6273f93934a8fac97
Sithum001 <128293308+Sithum001@users.noreply.github.com>
2026-01-28 12:41:24 +0530

    first commit

---
+++ client/src/pages/educator/MyCourse.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyCourse = () => {
+  return (
+    <div>
+      <h1>My Course Page</h1>
+    </div>
+  )
+}
+
+export default MyCourse
---
+++ client/src/pages/student/CourseDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CourseDetails = () => {
+  return (
+    <div>
+      <h1>Course detail Page</h1>
+    </div>
+  )
+}
+
+export default CourseDetails
