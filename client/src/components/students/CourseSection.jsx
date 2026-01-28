45a99efcbe220ee990a1821cc3db4c05fadfa2a1
Sithum001 <128293308+Sithum001@users.noreply.github.com>
2026-01-28 23:05:17 +0530

    hero section

--- client/src/components/students/CourseSection.jsx
+++ client/src/components/students/CourseSection.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const CourseSection = () => {
   return (
-    <div>
-      <h1>CourseSection</h1>
+    <div className='py-16 md:px-40 px-8'>
+      <h2 className='text-3xl font'></h2>
+    
     </div>
   )
 }
--- client/src/components/students/Hero.jsx
+++ client/src/components/students/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
+import SearchBar from './SearchBar'
 
 const Hero = () => {
   return (
@@ -9,7 +10,13 @@ const Hero = () => {
       Empower Your future with the courses designed to <span className='text-blue-600'>fit your choice.</span> <img src={assets.sketch} alt="sketch"
       className='md:block hidden absolute -bottom-7 right-0' /></h1>
 
-      <p></p>
+      <p className='md:block hidden text-gray-500 max-w-2xl mx-auto'>
+        We bring together world class instructors, interactive content, and a supportive community
+        to help you achieve your personal and professional goals</p>
+
+      <p className='md:hidden text-gray-500 max-w-sm mx-auto'> We bring together world class instructors, interactive content, and a supportive community
+        to help you achieve your personal and professional goals </p>
+        <SearchBar/>
     </div>
   )
 }
