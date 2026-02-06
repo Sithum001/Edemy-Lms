b08a986afbb7b07c104b83c81fb27f92e5609c34
Sithum001 <128293308+Sithum001@users.noreply.github.com>
2026-02-06 22:42:56 +0530

    educator sidebar

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../../components/educator/Navbar'
+import Sidebar from '../../components/educator/Sidebar'
 
 const Educator = () => {
   return (
     <div>
       <Navbar/>
       <h1>Educator page</h1>
-      <div>
-        {<Outlet/>}
+      <div className='flex'>
+        <Sidebar/>
+       <div className='flex-1'>
+         {<Outlet/>}
+       </div>
       </div>
     </div>
   )
