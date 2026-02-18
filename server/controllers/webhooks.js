6b3895922b709ddfa0bb27b6346e0dc23557097b
Sithum001 <128293308+Sithum001@users.noreply.github.com>
2026-02-18 18:12:15 +0530

    back end sever created

---
+++ server/controllers/webhooks.js
@@ -0,0 +1,5 @@
+import { Webhook } from "svix";
+import User from "../models/User.js"
+
+//API contoller function to manage clerk user with database
+
---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './configs/mongodb.js'
+
+//initialize express
+
+const app= express()
+
+//connect to db
+await connectDB()
+
+//middlewares
+app.use(cors())
+
+//routes
+app.get('/',(req,res)=>res.send("API Working"))
+
+//port
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
+
