d18fccbc886aa33f220d5d45d125dd517c6db3a7
Sithum001 <128293308+Sithum001@users.noreply.github.com>
2026-02-18 23:30:43 +0530

    back end sever created

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -3,3 +3,28 @@ import User from "../models/User.js"
 
 //API contoller function to manage clerk user with database
 
+export const clerkWebhooks = async (req, res)=>{
+    try {
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+
+        await whook.verify(JSON.stringify(req.body),{
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp":req.headers["svix-timestamp"],
+            "svix-signature":req.headers["svix-signature"]
+        })
+
+        const {data,type} = req.body
+
+        switch (key) {
+            case value:
+                
+                break;
+        
+            default:
+                break;
+        }
+
+    } catch (error) {
+        
+    }
+}
