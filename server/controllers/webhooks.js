cd55fac9a1a19afbc67509c7c299a5be177809e8
Sithum001 <128293308+Sithum001@users.noreply.github.com>
2026-02-24 15:18:43 +0530

    back end  webhooks

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -15,16 +15,45 @@ export const clerkWebhooks = async (req, res)=>{
 
         const {data,type} = req.body
 
-        switch (key) {
-            case value:
-                
+        switch (type) {
+            case 'user.created':{
+                const userData ={
+                    _id: data.id,
+                    email:data.email_address[0].email_address,
+                    name:data.first_name + " " + data.last_name,
+                    imageUrl: data.image_url,
+                }
+                await User.create(userData)
+                res.json({})
                 break;
+            }
+
+            case 'user.updated':{
+                const userData ={
+                    _id: data.id,
+                    email:data.email_address[0].email_address,
+                    name:data.first_name + " " + data.last_name,
+                    imageUrl: data.image_url,
+                }
+                await User.findByIdUpdate(data.id,userData)
+                res.json({})
+                break;
+            }
+
+               case 'user.deleted':{
+               
+                await User.findByIdAndDelete(data.id)
+                res.json({})
+                break;
+            }
+                
         
             default:
                 break;
         }
 
     } catch (error) {
+         res.json({success: false, message: error.message})
         
     }
 }
